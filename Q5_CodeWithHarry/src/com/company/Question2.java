fc0bfc8354c12509d949269730c24022e501eb97
NishchalTharayil <nishchaltharayil23@gmail.com>
2023-09-01 19:08:24 +0530

    Version-2

---
+++ Q5_CodeWithHarry/src/com/company/Question2.java
@@ -0,0 +1,19 @@
+package com.company;
+
+
+//Write a java program to replace spaces with Underscore
+
+public class Question2 {
+
+	public static void main(String[] args) 
+	{
+		String name = "Nishchal T S";
+		System.out.println(name.replace(" ", "_"));
+	}
+
+}
+
+
+// OUTPUT
+
+// Nishchal_T_S
